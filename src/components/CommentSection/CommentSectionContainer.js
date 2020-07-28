be74807cae66fd5d953126924f1fda7deac40301
CannaShaq <itscoldinwinter2@gmail.com>
2020-07-28 12:04:33 -0700

    test
    npm start not working

--- src/components/CommentSection/CommentSectionContainer.js
+++ src/components/CommentSection/CommentSectionContainer.js
@@ -1,4 +1,3 @@
-// You will add code to this file
 import React, { useState } from "react";
 import CommentInput from "./CommentInput";
 import Comment from "./Comment";
@@ -6,10 +5,12 @@ import "./Comment.css";
 
 const CommentSection = props => {
   // Add state for the comments
-
+  const[comments, setComment] = useState(props.comments);
   return (
     <div>
-      {/* map through the comments data and return the Comment component */}
+      {/* map through the comments data and return the Comment component */
+      comments.map((comment, index) => (<Comment key={index} comment ={comment}/>))
+      }
       <CommentInput />
     </div>
   );
--- src/components/PostsContainer/Post.js
+++ src/components/PostsContainer/Post.js
@@ -1,5 +1,4 @@
-// You will add code in this file
-import React from "react";
+import React, {useState} from "react";
 import CommentSection from "../CommentSection/CommentSectionContainer";
 import LikeSection from "./LikeSection";
 import PostHeader from "./PostHeader";
@@ -9,6 +8,7 @@ import "./Posts.css";
 // pass props in this file to
 const Post = props => {
   // set up state for the likes
+  const [likes, setLikes] = useState(props.likes);
 
   return (
     <div className="post-border">
@@ -35,5 +35,3 @@ const Post = props => {
 };
 
 export default Post;
-
-
